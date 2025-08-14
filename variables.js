d29c0954ac6a4c0131fd0ae9d3f9201253bb6e99
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-08-14 05:11:32 -0700

    feat: add TypeScript number and boolean type annotation examples

--- variables.js
+++ variables.js
@@ -1,2 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hi, World!";
+greetings = 6;
+greetings = true;
+var mynum = 6;
+mynum.toUpperCase();
+greetings.toLowercase();
 console.log(greetings);
+// number variable
+var userId = 12345;
+var userId2 = 67890.422;
+var userId3;
+userId3 = 1234567890; // not good practice, but valid
+userId.toFixed(5);
+var userId4 = 1234567890.12345;
+userId4.toFixed(2);
+userId4 = "hilal";
+// boolean variable
+var isLoggedIn = true;
+isLoggedIn.valueOf();
--- variables.ts
+++ variables.ts
@@ -10,4 +10,25 @@ mynum.toUpperCase()
 greetings.toLowercase();
 console.log(greetings);
 
+// number variable
+
+let userId: number = 12345;
+let userId2: number = 67890.422;
+
+let userId3: number
+userId3 = 1234567890; // not good practice, but valid
+
+userId.toFixed(5);
+
+let userId4 = 1234567890.12345;
+userId4.toFixed(2);
+userId4 = "hilal"
+
+// boolean variable
+let isLoggedIn: boolean = true;
+
+isLoggedIn.valueOf();
+
+
+
 export {}
