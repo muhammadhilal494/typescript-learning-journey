ee5a7fa419b106e59b38df32bfcf1b463fe207d6
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-08-14 04:26:01 -0700

    first commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+var user = {
+    name: "Hilal",
+    age: 25,
+    isActive: true,
+    hobbies: ["reading", "gaming", "coding"]
+};
+console.log("Hilal");
+console.log(user.name);
+console.log(user.age);
---
+++ intro.ts
@@ -0,0 +1,11 @@
+let user = {
+    name: "Hilal",
+    age: 25,
+    isActive: true,
+    hobbies: ["reading", "gaming", "coding"]
+}
+
+
+console.log("Hilal");
+console.log(user.name);
+console.log(user.age);
