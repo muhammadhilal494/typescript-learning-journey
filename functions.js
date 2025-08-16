77f663cbcf8449f4faf15293ce31ef5472c4fc6e
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-08-16 02:54:54 -0700

    feat: demonstrate TypeScript function return types with number, string, void, and never

--- functions.js
+++ functions.js
@@ -22,3 +22,30 @@ var loginUserS = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = true; }
 };
 loginUserS("hilal", "hilal@example"); // isPaid will be true by default
+function addTwo(num) {
+    return num + 2;
+    // return "hey"
+}
+addTwo(5); // error, return type is not defined
+// function getValue(myVal: number): boolean {
+//     if (myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+var getValue = function (myVal) {
+    return "";
+};
+// const heros = ["thor", "spiderman", "ironman"];
+var heros = [1, 2, 3]; // this is not a good practice
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+    // return 2;
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+    // return 2; // void function should not return anything
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- functions.ts
+++ functions.ts
@@ -33,6 +33,44 @@ let loginUserS = (name: string, email: string,
 
 loginUserS("hilal", "hilal@example"); // isPaid will be true by default
 
+function addTwo(num: number): number{
+     return num + 2;
+
+     // return "hey"
+}
+
+addTwo(5); // error, return type is not defined
+
+
+// function getValue(myVal: number): boolean {
+//     if (myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+
+const getValue = (myVal: number): string => {
+          return ""
+}
+
+// const heros = ["thor", "spiderman", "ironman"];
+const heros = [1, 2, 3]; // this is not a good practice
+heros.map((hero): string => {
+    return `hero is ${hero}`
+     // return 2;
+})
+
+
+function consoleError(errmsg: string): void {
+     console.log(errmsg);
+     // return 2; // void function should not return anything
+}
+
+function handleError(errmsg: string): never {
+     throw new Error(errmsg);
+}
+
+
 
 
 export {}
